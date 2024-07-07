c65ca964e588b025e100199570233d9b90513331
Zhikai Xu <3180111125@zju.edu.cn>
2024-07-07 10:30:26 +0800

    feat: 验证码服务创建并注册nacos

--- api-passenger/src/main/java/com/zhikaixu/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/zhikaixu/apipassenger/controller/VerificationCodeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableDiscoveryClient
 public class VerificationCodeController {
 
     @Autowired
