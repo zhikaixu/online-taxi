39e8860c55b8f2b345a7caf1ae2dcbcd73527ef5
Zhikai Xu <3180111125@zju.edu.cn>
2024-07-07 16:04:03 +0800

    feat: 返回值统一处理代码

---
+++ internal-common/src/main/java/com/zhikaixu/internalcommon/constant/CommonStatusEnum.java
@@ -0,0 +1,25 @@
+package com.zhikaixu.internalcommon.constant;
+
+import lombok.Getter;
+
+public enum CommonStatusEnum {
+    /**
+    * 成功
+    */
+    SUCCESS(1, "success"),
+    /**
+     * 失败
+     */
+    FAIL(0, "fail");
+
+    @Getter
+    private int code;
+
+    @Getter
+    private String value;
+
+    CommonStatusEnum(int code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+}
---
+++ internal-common/src/main/java/com/zhikaixu/internalcommon/response/NumberCodeResponse.java
@@ -0,0 +1,8 @@
+package com.zhikaixu.internalcommon.response;
+
+import lombok.Data;
+
+@Data
+public class NumberCodeResponse {
+    private int numberCode;
+}
