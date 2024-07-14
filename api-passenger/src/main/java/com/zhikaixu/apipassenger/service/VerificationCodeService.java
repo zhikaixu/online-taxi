2cf741e1f9798ab2f41083dcc1c9f5e24ce597cc
Zhikai Xu <3180111125@zju.edu.cn>
2024-07-14 17:29:33 +0800

    feat: 获取并校验Redis中的验证码

--- api-passenger/src/main/java/com/zhikaixu/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/zhikaixu/apipassenger/service/VerificationCodeService.java
@@ -1,11 +1,14 @@
 package com.zhikaixu.apipassenger.service;
 
+import com.alibaba.nacos.api.utils.StringUtils;
 import com.zhikaixu.apipassenger.remote.ServiceVerificationcodeClient;
+import com.zhikaixu.internalcommon.constant.CommonStatusEnum;
 import com.zhikaixu.internalcommon.dto.ResponseResult;
 import com.zhikaixu.internalcommon.response.NumberCodeResponse;
 import com.zhikaixu.internalcommon.response.TokenResponse;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -32,7 +35,7 @@ public class VerificationCodeService {
         ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationcodeClient.getNumberCode(6);
         int numberCode = numberCodeResponse.getData().getNumberCode();
         // 2. 将验证码存入Redis
-        stringRedisTemplate.opsForValue().set(verificationCodePrefix+passengerPhone, numberCode+"", 2, TimeUnit.MINUTES);
+        stringRedisTemplate.opsForValue().set(generateKeyByPhone(passengerPhone), numberCode+"", 2, TimeUnit.MINUTES);
 
         // 3. 通过短信服务商，将对应的验证码发送到手机上。
         return ResponseResult.success(null);
@@ -48,8 +51,21 @@ public class VerificationCodeService {
         // 1. 根据手机号，去Redis读取验证码
         System.out.println("根据手机号，去Redis读取验证码");
 
+        // 生成key
+        String key = generateKeyByPhone(passengerPhone);
+
+        // 根据key获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value: " + codeRedis);
+
         // 2. 校验验证码
         System.out.println("校验验证码");
+        if (StringUtils.isBlank(codeRedis)) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        if (!verificationCode.trim().equals(codeRedis.trim())) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
 
         // 3. 判断原来是否有用户，并做对应处理
         System.out.println("判断原来是否有用户，并做对应处理");
@@ -61,5 +77,9 @@ public class VerificationCodeService {
         tokenResponse.setToken("token");
         return ResponseResult.success(tokenResponse);
 //        return null;
+    }
+
+    private String generateKeyByPhone(String passengerPhone) {
+        return verificationCodePrefix + passengerPhone;
     }
 }
--- internal-common/src/main/java/com/zhikaixu/internalcommon/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/zhikaixu/internalcommon/constant/CommonStatusEnum.java
@@ -3,6 +3,11 @@ package com.zhikaixu.internalcommon.constant;
 import lombok.Getter;
 
 public enum CommonStatusEnum {
+
+    /**
+     * 验证码错误提示：1000-1099
+     */
+    VERIFICATION_CODE_ERROR(1099, "验证码不正确"),
     /**
     * 成功
     */
