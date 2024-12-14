d24c1dfbc3618b627a8ce22456608d985905cd5b
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-14 16:52:48 -0500

    feat 5: 增加乘客用户注册接口中插入用户逻辑(TESTED)

--- service-passenger-user/src/main/java/com/zhikaixu/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/zhikaixu/servicepassengeruser/service/UserService.java
@@ -6,6 +6,7 @@ import com.zhikaixu.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,14 +20,27 @@ public class UserService {
     public ResponseResult loginOrRegister(String passengerPhone) {
 
         System.out.println("user service被调用，手机号: " + passengerPhone);
+
         // 根据手机号查询用户信息
         Map<String, Object> map = new HashMap<>();
         map.put("passenger_phone", passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers.isEmpty()?"无记录":passengerUsers.get(0).getPassengerName());
-        // 判断用户信息是否存在
 
+        // 判断用户信息是否存在
         // 如果不存在，插入用户信息
+        if (passengerUsers.isEmpty()) {
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("zhikai");
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte) 0);
+
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+
+            passengerUserMapper.insert(passengerUser);
+        }
 
         return ResponseResult.success();
     }
