c3b8da43404e61e430baee5d60c63e7656264bf7
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-15 16:26:49 -0500

    feat 12: 增加token拦截器逻辑 (UNTESTED)

---
+++ api-passenger/src/main/java/com/zhikaixu/apipassenger/interceptor/JwtInterceptor.java
@@ -0,0 +1,48 @@
+package com.zhikaixu.apipassenger.interceptor;
+
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.internalcommon.util.JwtUtils;
+import org.json.JSONObject;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        boolean result = true;
+        String resultString = "";
+
+        String token = request.getHeader("Authorization");
+        try {
+            JwtUtils.parseToken(token);
+        } catch (SignatureVerificationException e) {
+            resultString = "token sign error";
+            result = false;
+        } catch (TokenExpiredException e) {
+            resultString = "token time out";
+            result = false;
+        } catch (AlgorithmMismatchException e) {
+            resultString = "token AlgorithmMismatchException";
+            result = false;
+        } catch (Exception e) {
+            resultString = "token invalid";
+            result = false;
+        }
+
+        if (!result) {
+            PrintWriter out = response.getWriter();
+            JSONObject jsonObject = new JSONObject(ResponseResult.fail(resultString));
+            out.print(jsonObject);
+        }
+
+        return result;
+    }
+}
