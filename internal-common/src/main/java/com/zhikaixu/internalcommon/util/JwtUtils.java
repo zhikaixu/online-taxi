e381c1886aa58808a318b2da1347fef4ce3a41d0
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-16 16:20:33 -0500

    feat 18-1: 增加 双token生成接口代码 的逻辑并且测试完成 (TESTED)

--- internal-common/src/main/java/com/zhikaixu/internalcommon/util/JwtUtils.java
+++ internal-common/src/main/java/com/zhikaixu/internalcommon/util/JwtUtils.java
@@ -28,6 +28,9 @@ public class JwtUtils {
     // 双token类型
     private static final String JWT_TOKEN_TYPE = "tokenType";
 
+    // tokenTime
+    private static final String JWT_TOKEN_TIME = "tokenTime";
+
     // 生成Token
     public static String generatorToken(String passengerPhone, String identity, String tokenType) {
         Map<String, String> map = new HashMap<>();
@@ -36,9 +39,7 @@ public class JwtUtils {
         map.put(JWT_TOKEN_TYPE, tokenType);
 
         // token过期时间
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, 1);
-        Date date = calendar.getTime();
+        map.put(JWT_TOKEN_TIME, Calendar.getInstance().getTime().toString());
 
         JWTCreator.Builder builder = JWT.create();
         // 整合map
@@ -77,9 +78,9 @@ public class JwtUtils {
         try {
             tokenResult = JwtUtils.parseToken(token);
         } catch (Exception e) {
-
+//            return null;
         }
-        return null;
+        return tokenResult;
     }
 
 
