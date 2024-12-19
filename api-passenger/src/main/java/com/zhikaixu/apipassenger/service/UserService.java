910e2a797df1ea38419fef70e1de31767b5682b1
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-18 20:54:59 -0500

    feat 20: 增加 service-passenger-user根据手机号查询用户信息 接口 (TESTED)

--- api-passenger/src/main/java/com/zhikaixu/apipassenger/service/UserService.java
+++ api-passenger/src/main/java/com/zhikaixu/apipassenger/service/UserService.java
@@ -2,6 +2,8 @@ package com.zhikaixu.apipassenger.service;
 
 import com.zhikaixu.internalcommon.dto.PassengerUser;
 import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.internalcommon.dto.TokenResult;
+import com.zhikaixu.internalcommon.util.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,12 @@ public class UserService {
     public ResponseResult getUserByAccessToken(String accessToken) {
         log.info("accessToken:" + accessToken);
         // 解析accessToken，拿到手机号
-
+        TokenResult tokenResult = JwtUtils.checkToken(accessToken);
+        String phone = tokenResult.getPhone(); // TODO: need check null?
+        log.info("手机号: " + phone);
         // 根据手机号查询用户信息
+        // 调用service-passenger-user的接口
+
 
         PassengerUser passengerUser = new PassengerUser();
         passengerUser.setPassengerName("张三");
