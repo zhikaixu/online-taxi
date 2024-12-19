3bc8ba80cb60e2b50739db1f0440fc37d6b9a324
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-19 13:56:51 -0500

    feat 24: 增加 service-price服务代码骨架 (TESTED)

---
+++ service-price/src/main/java/com/zhikaixu/serviceprice/controller/ForecastPriceController.java
@@ -0,0 +1,26 @@
+package com.zhikaixu.serviceprice.controller;
+
+import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.internalcommon.request.ForecastPriceDTO;
+import com.zhikaixu.serviceprice.service.ForecastPriceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ForecastPriceController {
+
+    @Autowired
+    private ForecastPriceService forecastPriceService;
+
+    @PostMapping("/forecast-price")
+    public ResponseResult forecastPrice(@RequestBody ForecastPriceDTO forecastPriceDTO) {
+        String depLongitude =  forecastPriceDTO.getDepLongitude();
+        String depLatitude = forecastPriceDTO.getDepLatitude();
+        String destLongitude = forecastPriceDTO.getDestLongitude();
+        String destLatitude = forecastPriceDTO.getDestLatitude();
+
+        return forecastPriceService.forecastPrice(depLongitude, depLatitude, destLongitude, destLatitude);
+    }
+}
