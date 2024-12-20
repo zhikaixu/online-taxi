020529ad30a005307fa59ef538c2b351a3ba482f
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-20 16:10:52 -0500

    feat 31: 完成service-price读取计价规则逻辑 (TESTED)

--- internal-common/src/main/java/com/zhikaixu/internalcommon/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/zhikaixu/internalcommon/constant/CommonStatusEnum.java
@@ -20,6 +20,11 @@ public enum CommonStatusEnum {
     USER_NOT_EXISTS(1200, "当前用户不存在"),
 
     /**
+     * 计价规则: 1300-1399
+     */
+    PRICE_RULE_EMPTY(1300, "计价规则不存在"),
+
+    /**
     * 成功
     */
     SUCCESS(1, "success"),
