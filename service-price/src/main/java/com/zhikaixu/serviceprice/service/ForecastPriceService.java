9384f617bf3d358e6e523ce18cc5964b3b4ecfdf
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-20 16:10:52 -0500

    feat 31: 完成service-price读取计价规则逻辑 (TESTED)

--- service-price/src/main/java/com/zhikaixu/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/zhikaixu/serviceprice/service/ForecastPriceService.java
@@ -1,17 +1,28 @@
 package com.zhikaixu.serviceprice.service;
 
+import com.zhikaixu.internalcommon.constant.CommonStatusEnum;
+import com.zhikaixu.internalcommon.dto.PriceRule;
 import com.zhikaixu.internalcommon.dto.ResponseResult;
 import com.zhikaixu.internalcommon.request.ForecastPriceDTO;
 import com.zhikaixu.internalcommon.response.DirectionResponse;
 import com.zhikaixu.internalcommon.response.ForecastPriceResponse;
+import com.zhikaixu.serviceprice.mapper.PriceRuleMapper;
 import com.zhikaixu.serviceprice.remote.ServiceMapClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class ForecastPriceService {
     private final ServiceMapClient serviceMapClient;
+
+    @Autowired
+    private PriceRuleMapper priceRuleMapper;
 
     public ForecastPriceService(ServiceMapClient serviceMapClient) {
         this.serviceMapClient = serviceMapClient;
@@ -36,6 +47,15 @@ public class ForecastPriceService {
         log.info("距离: " + distance + ", 时长: " + duration);
 
         log.info("读取计价规则");
+        Map<String, Object> queryMap = new HashMap<>();
+        queryMap.put("city_code", "110000");
+        queryMap.put("vehicle_type", "1");
+        List<PriceRule> priceRules = priceRuleMapper.selectByMap(queryMap);
+        if (priceRules.isEmpty()) {
+            return ResponseResult.fail(CommonStatusEnum.PRICE_RULE_EMPTY.getCode(), CommonStatusEnum.PRICE_RULE_EMPTY.getValue());
+        }
+
+        PriceRule priceRule = priceRules.get(0);
 
         log.info("根据距离、时长和计价规则，计算价格");
 
