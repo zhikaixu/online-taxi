2eac9f2cbe11950cf14f00773f73d3493835acc9
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-21 16:32:23 -0500

    feat 37: 地区字典获取并成功入库 (TESTED)

--- service-map/src/main/java/com/zhikaixu/servicemap/service/DicDistrictService.java
+++ service-map/src/main/java/com/zhikaixu/servicemap/service/DicDistrictService.java
@@ -1,10 +1,14 @@
 package com.zhikaixu.servicemap.service;
 
 import com.zhikaixu.internalcommon.constant.AmapConfigConstants;
+import com.zhikaixu.internalcommon.constant.CommonStatusEnum;
+import com.zhikaixu.internalcommon.dto.DicDistrict;
 import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.servicemap.mapper.DicDistrictMapper;
 import com.zhikaixu.servicemap.remote.MapDicDistrictClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,15 +17,94 @@ public class DicDistrictService {
     @Autowired
     private MapDicDistrictClient mapDicDistrictClient;
 
+    @Autowired
+    private DicDistrictMapper dicDistrictMapper;
+
     public ResponseResult initDicDistrict(String keywords) {
 
         // 请求地图
-        String dicDistrict = mapDicDistrictClient.dicDistrict(keywords);
-        System.out.println(dicDistrict);
-        // 解析结果
+        String dicDistrictResult = mapDicDistrictClient.dicDistrict(keywords);
 
+        // 解析结果
+        JSONObject dicDistrictJsonObject = new JSONObject(dicDistrictResult);
+        int status = dicDistrictJsonObject.getInt(AmapConfigConstants.STATUS);
+        if (status != 1) {
+            return ResponseResult.fail(CommonStatusEnum.MAP_DISTRICT_ERROR.getCode(), CommonStatusEnum.MAP_DISTRICT_ERROR.getValue());
+        }
+        JSONArray countryJsonArray = dicDistrictJsonObject.getJSONArray(AmapConfigConstants.DISTRICTS);
+        for (int i = 0; i < countryJsonArray.length(); i++) {
+            JSONObject countryJsonObject = countryJsonArray.getJSONObject(i);
+            String countryAddressCode = countryJsonObject.getString(AmapConfigConstants.ADCODE);
+            String countryAddressName = countryJsonObject.getString(AmapConfigConstants.NAME);
+            String countryParentAddressCode = "0";
+            String countryLevel = countryJsonObject.getString(AmapConfigConstants.LEVEL);
+
+            insertDicDistrict(countryAddressCode, countryAddressName, countryLevel, countryParentAddressCode);
+
+            JSONArray provinceJsonArray = countryJsonObject.getJSONArray(AmapConfigConstants.DISTRICTS);
+            for (int p = 0; p < provinceJsonArray.length(); p++) {
+                JSONObject provinceJsonObject = provinceJsonArray.getJSONObject(p);
+                String provinceAddressCode = provinceJsonObject.getString(AmapConfigConstants.ADCODE);
+                String provinceAddressName = provinceJsonObject.getString(AmapConfigConstants.NAME);
+                String provinceParentAddressCode = countryAddressCode;
+                String provinceLevel = provinceJsonObject.getString(AmapConfigConstants.LEVEL);
+
+                insertDicDistrict(provinceAddressCode, provinceAddressName, provinceLevel, provinceParentAddressCode);
+
+                JSONArray cityJsonArray = provinceJsonObject.getJSONArray(AmapConfigConstants.DISTRICTS);
+                for (int c = 0; c < cityJsonArray.length(); c++) {
+                    JSONObject cityJsonObject = cityJsonArray.getJSONObject(c);
+                    String cityAddressCode = cityJsonObject.getString(AmapConfigConstants.ADCODE);
+                    String cityAddressName = cityJsonObject.getString(AmapConfigConstants.NAME);
+                    String cityParentAddressCode = provinceAddressCode;
+                    String cityLevel = cityJsonObject.getString(AmapConfigConstants.LEVEL);
+
+                    insertDicDistrict(cityAddressCode, cityAddressName, cityLevel, cityParentAddressCode);
+
+                    JSONArray districtJsonArray = cityJsonObject.getJSONArray(AmapConfigConstants.DISTRICTS);
+                    for (int d = 0; d < districtJsonArray.length(); d++) {
+                        JSONObject districtJsonObject = districtJsonArray.getJSONObject(d);
+                        String districtAddressCode = districtJsonObject.getString(AmapConfigConstants.ADCODE);
+                        String districtAddressName = districtJsonObject.getString(AmapConfigConstants.NAME);
+                        String districtParentAddressCode = cityAddressCode;
+                        String districtLevel = districtJsonObject.getString(AmapConfigConstants.LEVEL);
+
+                        if (districtLevel.equals(AmapConfigConstants.STREET)) {
+                            continue;
+                        }
+                        insertDicDistrict(districtAddressCode, districtAddressName, districtLevel, districtParentAddressCode);
+                    }
+                }
+            }
+        }
         // 插入数据库
 
         return ResponseResult.success();
+    }
+
+    public void insertDicDistrict(String addressCode, String addressName, String level, String parentAddressCode) {
+        DicDistrict district = new DicDistrict();
+        district.setAddressCode(addressCode);
+        district.setAddressName(addressName);
+        int levelInt = generateLevel(level);
+        district.setLevel(levelInt);
+
+        district.setParentAddressCode(parentAddressCode);
+
+        dicDistrictMapper.insert(district);
+    }
+
+    public int generateLevel(String level) {
+        int levelInt = 0;
+        if (level.trim().equals("country")) {
+            levelInt = 0;
+        } else if (level.trim().equals("province")) {
+            levelInt = 1;
+        } else if (level.trim().equals("city")) {
+            levelInt = 2;
+        } else if (level.trim().equals("district")) {
+            levelInt = 3;
+        }
+        return levelInt;
     }
 }
