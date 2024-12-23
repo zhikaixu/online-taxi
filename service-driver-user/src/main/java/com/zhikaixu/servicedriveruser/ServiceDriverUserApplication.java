ca35f1a178c27b23005015a877de6ce029e61c0b
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-23 15:44:35 -0500

    feat 40: service-driver-user注入nacos

--- service-driver-user/src/main/java/com/zhikaixu/servicedriveruser/ServiceDriverUserApplication.java
+++ service-driver-user/src/main/java/com/zhikaixu/servicedriveruser/ServiceDriverUserApplication.java
@@ -3,8 +3,10 @@ package com.zhikaixu.servicedriveruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.zhikaixu.servicedriveruser.mapper")
 public class ServiceDriverUserApplication {
     public static void main(String[] args) {
