e50ccebabc09a339844c762a0f1a4f0292be83fb
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-23 16:53:37 -0500

    feat 46: 完成 api-driver注入nacos (TESTED)

--- api-driver/src/main/java/com/zhikaixu/apidriver/ApiDriverApplication.java
+++ api-driver/src/main/java/com/zhikaixu/apidriver/ApiDriverApplication.java
@@ -2,11 +2,13 @@ package com.zhikaixu.apidriver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ApiDriverApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiDriverApplication.class);
