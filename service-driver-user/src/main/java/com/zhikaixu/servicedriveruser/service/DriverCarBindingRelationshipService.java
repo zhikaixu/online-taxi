807bc91e38a0e84d9a8d2d82b107bcae3df66dc7
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-24 15:05:04 -0500

    feat 51: service-driver-user 司机车辆绑定代码完善 (TESTED)

--- service-driver-user/src/main/java/com/zhikaixu/servicedriveruser/service/DriverCarBindingRelationshipService.java
+++ service-driver-user/src/main/java/com/zhikaixu/servicedriveruser/service/DriverCarBindingRelationshipService.java
@@ -1,6 +1,8 @@
 package com.zhikaixu.servicedriveruser.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhikaixu.internalcommon.constant.CommonStatusEnum;
 import com.zhikaixu.internalcommon.constant.DriverCarConstants;
 import com.zhikaixu.internalcommon.dto.DriverCarBindingRelationship;
 import com.zhikaixu.internalcommon.dto.ResponseResult;
@@ -26,6 +28,30 @@ public class DriverCarBindingRelationshipService {
     private DriverCarBindingRelationshipMapper driverCarBindingRelationshipMapper;
 
     public ResponseResult bind(DriverCarBindingRelationship driverCarBindingRelationship) {
+        // 判断：如果参数重的车辆和司机，已经做过绑定，则不允许再次绑定
+        QueryWrapper<DriverCarBindingRelationship> queryWrapper = new QueryWrapper<>();
+        // 司机被绑定了
+        queryWrapper.eq("driver_id", driverCarBindingRelationship.getDriverId());
+        // 遇到问题：加了判断之后，仍然可以重复绑定
+        // 通过在yml文件中配置mybatis的日志记录，将日志打印到控制台，发现sql语句的state为null，说明传入的driverCarBindingRelationship没有status
+        // 因此需要手动加上
+        queryWrapper.eq("bind_state", DriverCarConstants.DRIVER_CAR_BIND);
+
+        Integer integer = driverCarBindingRelationshipMapper.selectCount(queryWrapper);
+        if ((integer.intValue() > 0)) {
+            return ResponseResult.fail(CommonStatusEnum.DRIVER_BIND_EXISTS.getCode(), CommonStatusEnum.DRIVER_BIND_EXISTS.getValue());
+        }
+
+        // 车辆被绑定了
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("car_id", driverCarBindingRelationship.getCarId());
+        queryWrapper.eq("bind_state", DriverCarConstants.DRIVER_CAR_BIND);
+
+        integer = driverCarBindingRelationshipMapper.selectCount(queryWrapper);
+        if ((integer.intValue() > 0)) {
+            return ResponseResult.fail(CommonStatusEnum.CAR_BIND_EXISTS.getCode(), CommonStatusEnum.CAR_BIND_EXISTS.getValue());
+        }
+
         LocalDateTime now = LocalDateTime.now();
         driverCarBindingRelationship.setBindingTime(now);
 
