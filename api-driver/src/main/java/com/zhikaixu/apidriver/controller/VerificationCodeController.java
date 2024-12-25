2db073f987aee935c147e690d87ef2e5450195f3
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-25 15:53:35 -0500

    feat 60: api-driver服务 完成司机验证验证码功能 (TESTED)

--- api-driver/src/main/java/com/zhikaixu/apidriver/controller/VerificationCodeController.java
+++ api-driver/src/main/java/com/zhikaixu/apidriver/controller/VerificationCodeController.java
@@ -6,6 +6,7 @@ import com.zhikaixu.internalcommon.request.VerificationCodeDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,15 @@ public class VerificationCodeController {
         String driverPhone = verificationCodeDTO.getDriverPhone();
         log.info("driverPhone: " + driverPhone);
         return verificationCodeService.checkAndSendVerification(driverPhone);
+    }
+
+    @PostMapping("/verification-code-check")
+    public ResponseResult checkVerificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO) {
+        String driverPhone = verificationCodeDTO.getDriverPhone();
+        String verificationCode = verificationCodeDTO.getVerificationCode();
+
+        System.out.println("手机号: " + driverPhone + " 验证码: " + verificationCode);
+
+        return verificationCodeService.checkCode(driverPhone, verificationCode);
     }
 }
--- api-driver/src/main/java/com/zhikaixu/apidriver/service/VerificationCodeService.java
+++ api-driver/src/main/java/com/zhikaixu/apidriver/service/VerificationCodeService.java
@@ -1,13 +1,18 @@
 package com.zhikaixu.apidriver.service;
 
+import com.alibaba.nacos.api.utils.StringUtils;
 import com.zhikaixu.apidriver.remote.ServiceDriverUserClient;
 import com.zhikaixu.apidriver.remote.ServiceVerificationCodeClient;
 import com.zhikaixu.internalcommon.constant.CommonStatusEnum;
 import com.zhikaixu.internalcommon.constant.DriverCarConstants;
 import com.zhikaixu.internalcommon.constant.IdentityConstant;
+import com.zhikaixu.internalcommon.constant.TokenConstants;
 import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.internalcommon.request.VerificationCodeDTO;
 import com.zhikaixu.internalcommon.response.DriverUserExistsResponse;
 import com.zhikaixu.internalcommon.response.NumberCodeResponse;
+import com.zhikaixu.internalcommon.response.TokenResponse;
+import com.zhikaixu.internalcommon.util.JwtUtils;
 import com.zhikaixu.internalcommon.util.RedisPrefixUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,5 +59,49 @@ public class VerificationCodeService {
         stringRedisTemplate.opsForValue().set(key, numberCode+"", 2, TimeUnit.MINUTES);
 
         return ResponseResult.success("");
+    }
+
+    /**
+     * 校验验证码
+     * @param passengerPhone
+     * @param verificationCode
+     * @return
+     */
+    public ResponseResult checkCode(String driverPhone, String verificationCode) {
+        // 1. 根据手机号，去Redis读取验证码
+        System.out.println("根据手机号，去Redis读取验证码");
+
+        // 生成key
+        String key = RedisPrefixUtils.generateKeyByPhone(driverPhone, IdentityConstant.DRIVER_IDENTITY);
+
+        // 根据key获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value: " + codeRedis);
+
+        // 2. 校验验证码
+        System.out.println("校验验证码");
+        if (StringUtils.isBlank(codeRedis)) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        if (!verificationCode.trim().equals(codeRedis.trim())) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+
+        // 3. 颁发令牌
+        String accessToken = JwtUtils.generatorToken(driverPhone, IdentityConstant.DRIVER_IDENTITY, TokenConstants.ACCESS_TOKEN_TYPE);
+        String refreshToken = JwtUtils.generatorToken(driverPhone, IdentityConstant.DRIVER_IDENTITY, TokenConstants.REFRESH_TOKEN_TYPE);
+
+        // 将token存入redis中
+        String accessTokenKey = RedisPrefixUtils.generateTokenKey(driverPhone, IdentityConstant.DRIVER_IDENTITY, TokenConstants.ACCESS_TOKEN_TYPE);
+        stringRedisTemplate.opsForValue().set(accessTokenKey, accessToken, 30, TimeUnit.DAYS);
+
+        String refreshTokenKey = RedisPrefixUtils.generateTokenKey(driverPhone, IdentityConstant.DRIVER_IDENTITY, TokenConstants.REFRESH_TOKEN_TYPE);
+        stringRedisTemplate.opsForValue().set(refreshTokenKey, refreshToken, 31, TimeUnit.DAYS);
+
+        // 4. 响应
+        TokenResponse tokenResponse = new TokenResponse();
+        tokenResponse.setAccessToken(accessToken);
+        tokenResponse.setRefreshToken(refreshToken);
+        return ResponseResult.success(tokenResponse);
     }
 }
