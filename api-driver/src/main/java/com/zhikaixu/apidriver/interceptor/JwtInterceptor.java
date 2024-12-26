cab477574e8a7a8d2c6fa64a3ed1ea318e8360b7
Zhikai Xu <3180111125@zju.edu.cn>
2024-12-26 10:55:58 -0500

    feat 61: 司机端校验accessToken代码实现 (TESTED)

---
+++ api-driver/src/main/java/com/zhikaixu/apidriver/interceptor/JwtInterceptor.java
@@ -0,0 +1,58 @@
+package com.zhikaixu.apidriver.interceptor;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.zhikaixu.internalcommon.constant.TokenConstants;
+import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.internalcommon.dto.TokenResult;
+import com.zhikaixu.internalcommon.util.JwtUtils;
+import com.zhikaixu.internalcommon.util.RedisPrefixUtils;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        boolean result = true;
+        String resultString = "";
+
+        // 解析token
+        String token = request.getHeader("Authorization");
+        TokenResult tokenResult = JwtUtils.checkToken(token);
+
+        // 从redis中取出token
+        if (tokenResult == null) {
+            resultString = "token invalid";
+            result = false;
+        } else {
+            // 拼接key
+            String phone = tokenResult.getPhone();
+            String identity = tokenResult.getIdentity();
+            String tokenKey = RedisPrefixUtils.generateTokenKey(phone, identity, TokenConstants.ACCESS_TOKEN_TYPE);
+            // 从redis中取出token
+            String tokenRedis = stringRedisTemplate.opsForValue().get(tokenKey);
+            if (StringUtils.isBlank(tokenRedis) || (!token.trim().equals(tokenRedis.trim()))) {
+                resultString = "token invalid";
+                result = false;
+            }
+        }
+
+        if (!result) {
+            PrintWriter out = response.getWriter();
+            JSONObject jsonObject = new JSONObject(ResponseResult.fail(resultString));
+            out.print(jsonObject);
+        }
+
+        return result;
+    }
+}
