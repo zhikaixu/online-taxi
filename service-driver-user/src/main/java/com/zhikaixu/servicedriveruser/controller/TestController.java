943d2bf19e030234b29d48783a0ebd43124d9741
Zhikai Xu <3180111125@zju.edu.cn>
2025-01-01 16:03:19 -0500

    feat 92: mybatis-plus使用自定义sql查询测试 (TESTED)

--- service-driver-user/src/main/java/com/zhikaixu/servicedriveruser/controller/TestController.java
+++ service-driver-user/src/main/java/com/zhikaixu/servicedriveruser/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zhikaixu.servicedriveruser.controller;
 
 import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.servicedriveruser.mapper.DriverUserMapper;
 import com.zhikaixu.servicedriveruser.service.DriverUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,9 @@ public class TestController {
     @Autowired
     private DriverUserService driverUserService;
 
+    @Autowired
+    private DriverUserMapper driverUserMapper;
+
     @GetMapping("/test")
     public String test() {
         return "service-driver-user";
@@ -20,5 +24,11 @@ public class TestController {
     @GetMapping("/test-db")
     public ResponseResult testDb() {
         return driverUserService.testGetDriverUser();
+    }
+
+    // 测试mapper中的xml是否正常使用
+    @GetMapping("/test-xml")
+    public int testXml(String args) {
+        return driverUserMapper.select1("1");
     }
 }
