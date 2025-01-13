5a14760b694b91791ed06e91bda25b88e1bb2ef5
Zhikai Xu <3180111125@zju.edu.cn>
2025-01-13 15:08:20 -0500

    feat 147: api-passenger 验证码调用服务异常处理&redis事务处理

--- api-passenger/src/main/java/com/zhikaixu/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/zhikaixu/apipassenger/service/VerificationCodeService.java
@@ -13,6 +13,9 @@ import com.zhikaixu.internalcommon.response.TokenResponse;
 import com.zhikaixu.internalcommon.util.JwtUtils;
 import com.zhikaixu.internalcommon.util.RedisPrefixUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -54,45 +57,72 @@ public class VerificationCodeService {
      * @return
      */
     public ResponseResult checkCode(String passengerPhone, String verificationCode) {
-        // 1. 根据手机号，去Redis读取验证码
-        System.out.println("根据手机号，去Redis读取验证码");
-
-        // 生成key
-        String key = RedisPrefixUtils.generateKeyByPhone(passengerPhone, IdentityConstant.PASSENGER_IDENTITY);
-
-        // 根据key获取value
-        String codeRedis = stringRedisTemplate.opsForValue().get(key);
-        System.out.println("redis中的value: " + codeRedis);
-
-        // 2. 校验验证码
-        System.out.println("校验验证码");
-        if (StringUtils.isBlank(codeRedis)) {
-            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
-        }
-        if (!verificationCode.trim().equals(codeRedis.trim())) {
-            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
-        }
-
-        // 3. 判断原来是否有用户，并做对应处理
-        VerificationCodeDTO verificationCodeDTO = new VerificationCodeDTO();
-        verificationCodeDTO.setPassengerPhone(passengerPhone);
-        servicePassengerUserClient.loginOrRegister(verificationCodeDTO);
-
+//        // 1. 根据手机号，去Redis读取验证码
+//        System.out.println("根据手机号，去Redis读取验证码");
+//
+//        // 生成key
+//        String key = RedisPrefixUtils.generateKeyByPhone(passengerPhone, IdentityConstant.PASSENGER_IDENTITY);
+//
+//        // 根据key获取value
+//        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+//        System.out.println("redis中的value: " + codeRedis);
+//
+//        // 2. 校验验证码
+//        System.out.println("校验验证码");
+//        if (StringUtils.isBlank(codeRedis)) {
+//            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+//        }
+//        if (!verificationCode.trim().equals(codeRedis.trim())) {
+//            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+//        }
+//
+//        // 3. 判断原来是否有用户，并做对应处理
+//        VerificationCodeDTO verificationCodeDTO = new VerificationCodeDTO();
+//        verificationCodeDTO.setPassengerPhone(passengerPhone);
+//        try {
+//            servicePassengerUserClient.loginOrRegister(verificationCodeDTO);
+//        } catch (RuntimeException e) {
+//            return ResponseResult.fail(CommonStatusEnum.CALL_USER_ADD_ERROR.getCode(), CommonStatusEnum.CALL_USER_ADD_ERROR.getValue());
+//        }
         // 4. 颁发令牌
         String accessToken = JwtUtils.generatorToken(passengerPhone, IdentityConstant.PASSENGER_IDENTITY, TokenConstants.ACCESS_TOKEN_TYPE);
         String refreshToken = JwtUtils.generatorToken(passengerPhone, IdentityConstant.PASSENGER_IDENTITY, TokenConstants.REFRESH_TOKEN_TYPE);
+        // 开启redis事务
+        stringRedisTemplate.setEnableTransactionSupport(true);
+        SessionCallback<Boolean> callback = new SessionCallback<Boolean>() {
+            @Override
+            public Boolean execute(RedisOperations operations) throws DataAccessException {
+                // 事务开始
+                stringRedisTemplate.multi();
+                try {
+                    // 将token存入redis中
+                    String accessTokenKey = RedisPrefixUtils.generateTokenKey(passengerPhone, IdentityConstant.PASSENGER_IDENTITY, TokenConstants.ACCESS_TOKEN_TYPE);
+                    operations.opsForValue().set(accessTokenKey, accessToken, 30, TimeUnit.DAYS);
+                    int i = 1 / 0;
+                    String refreshTokenKey = RedisPrefixUtils.generateTokenKey(passengerPhone, IdentityConstant.PASSENGER_IDENTITY, TokenConstants.REFRESH_TOKEN_TYPE);
+                    operations.opsForValue().set(refreshTokenKey, refreshToken, 31, TimeUnit.DAYS);
+                    operations.exec();
+                    return true;
+                } catch (Exception e) {
+                    operations.discard();
+                    return false;
+                }
 
-        // 将token存入redis中
-        String accessTokenKey = RedisPrefixUtils.generateTokenKey(passengerPhone, IdentityConstant.PASSENGER_IDENTITY, TokenConstants.ACCESS_TOKEN_TYPE);
-        stringRedisTemplate.opsForValue().set(accessTokenKey, accessToken, 30, TimeUnit.DAYS);
+            }
+        };
 
-        String refreshTokenKey = RedisPrefixUtils.generateTokenKey(passengerPhone, IdentityConstant.PASSENGER_IDENTITY, TokenConstants.REFRESH_TOKEN_TYPE);
-        stringRedisTemplate.opsForValue().set(refreshTokenKey, refreshToken, 31, TimeUnit.DAYS);
+        Boolean execute = stringRedisTemplate.execute(callback);
+        System.out.println("事务提交or回滚: " + execute);
 
-        // 5. 响应
-        TokenResponse tokenResponse = new TokenResponse();
-        tokenResponse.setAccessToken(accessToken);
-        tokenResponse.setRefreshToken(refreshToken);
-        return ResponseResult.success(tokenResponse);
+        if (execute) {
+            // 5. 响应
+            TokenResponse tokenResponse = new TokenResponse();
+            tokenResponse.setAccessToken(accessToken);
+            tokenResponse.setRefreshToken(refreshToken);
+            return ResponseResult.success(tokenResponse);
+        } else {
+            return ResponseResult.fail(CommonStatusEnum.CHECK_CODE_ERROR.getCode(), CommonStatusEnum.CHECK_CODE_ERROR.getValue());
+        }
+
     }
 }
