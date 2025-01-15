c081991d0685c2fb509bda7e9f695b5670461f8b
Zhikai Xu <3180111125@zju.edu.cn>
2025-01-14 19:14:26 -0500

    feat 150: api-driver 司机抢单接口controller实现 (TESTED)

--- api-driver/src/main/java/com/zhikaixu/apidriver/controller/OrderController.java
+++ api-driver/src/main/java/com/zhikaixu/apidriver/controller/OrderController.java
@@ -2,9 +2,13 @@ package com.zhikaixu.apidriver.controller;
 
 import com.zhikaixu.apidriver.service.ApiDriverOrderInfoService;
 import com.zhikaixu.internalcommon.dto.ResponseResult;
+import com.zhikaixu.internalcommon.dto.TokenResult;
 import com.zhikaixu.internalcommon.request.OrderRequest;
+import com.zhikaixu.internalcommon.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/order")
@@ -12,6 +16,24 @@ public class OrderController {
 
     @Autowired
     private ApiDriverOrderInfoService apiDriverOrderInfoService;
+
+    /**
+     * 司机抢单
+     * @param orderRequest
+     * @param httpServletRequest
+     * @return
+     */
+    @PostMapping("/grab")
+    public ResponseResult grab(@RequestBody OrderRequest orderRequest, HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader("Authorization");
+
+        TokenResult tokenResult = JwtUtils.parseToken(token);
+        String identity = tokenResult.getIdentity();
+        String phone = tokenResult.getPhone();
+        Long orderId = orderRequest.getOrderId();
+
+        return ResponseResult.success("");
+    }
 
     /**
      * 司机去接乘客
