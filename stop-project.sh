92c52cd5e795761f012a218a0867e00188b3b0a6
Zhikai Xu <3180111125@zju.edu.cn>
2025-01-20 23:13:50 -0500

    feat 169: zookeeper 环境搭建

--- stop-project.sh
+++ stop-project.sh
@@ -8,3 +8,6 @@ redis-cli -p 6381 shutdown
 redis-cli -p 26379 shutdown
 redis-cli -p 26380 shutdown
 redis-cli -p 26381 shutdown
+
+#zookeeper
+/Users/zhikaixu/Desktop/apache-zookeeper-3.8.4-bin/bin/zkServer.sh stop
