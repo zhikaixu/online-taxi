a2605e74d0a22da5dbb76aa8ac7175756f2ea7a6
Zhikai Xu <3180111125@zju.edu.cn>
2025-03-03 16:17:15 -0500

    fix: delete something

--- service-pay/src/main/java/com/zhikaixu/pay/controller/AlipayController.java
+++ service-pay/src/main/java/com/zhikaixu/pay/controller/AlipayController.java
@@ -60,10 +60,6 @@ public class AlipayController {
 
         return "success";
     }
-
-    @GetMapping("/huchenhao")
-    public String testMaoMao(HttpServletRequest request) {
-        return "阿琛琛博晨勃勃起烂琛";
-    }
+    
 
 }
